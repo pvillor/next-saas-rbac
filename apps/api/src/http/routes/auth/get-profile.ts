c15a8586d04b46f5a2ee4d5aad9d09a5595a74d2
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-06-05 15:14:51 -0400

    feat: get-profile route

---
+++ apps/api/src/http/routes/auth/get-profile.ts
@@ -0,0 +1,52 @@
+import { prisma } from '@/lib/prisma'
+import type { FastifyInstance } from 'fastify'
+import type { ZodTypeProvider } from 'fastify-type-provider-zod'
+import z from 'zod'
+
+export async function getProfile(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    '/profile',
+    {
+      schema: {
+        tags: ['auth'],
+        summary: 'Get authenticated user profile',
+        response: {
+          200: z.object({
+            user: z.object({
+              id: z.string().uuid(),
+              name: z.string().nullable(),
+              email: z.string().email(),
+              avatarUrl: z.string().url().nullable(),
+            }),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { sub } = await request.jwtVerify<{ sub: string }>()
+
+      // Ensure sub is defined
+      if (!sub) {
+        throw new Error('Invalid token')
+      }
+
+      const user = await prisma.user.findUnique({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          avatarUrl: true,
+        },
+        where: {
+          id: sub,
+        },
+      })
+
+      if (!user) {
+        throw new Error('User not found')
+      }
+
+      return reply.send({ user })
+    }
+  )
+}
--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,20 +1,51 @@
 import fastifyCors from '@fastify/cors'
 import fastify from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { createAccount } from './routes/auth/create-account'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
+import { authenticateWithPassword } from './routes/auth/authenticate-with-password'
+import fastifyJwt from '@fastify/jwt'
+import { getProfile } from './routes/auth/get-profile'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Next.js SaaS',
+      description: 'Full-stack SaaS app with multi-tenant & RBAC',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(fastifyJwt, {
+  secret: 'my-jwt-secret',
+})
+
 app.register(fastifyCors)
 
 app.register(createAccount)
+app.register(authenticateWithPassword)
+app.register(getProfile)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Link Start')
