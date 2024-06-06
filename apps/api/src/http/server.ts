f6d9fc5405f909c73bb30d2f414e148fce5bd13c
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-06-06 09:26:43 -0400

    feat: reset password

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -13,6 +13,8 @@ import { authenticateWithPassword } from './routes/auth/authenticate-with-passwo
 import fastifyJwt from '@fastify/jwt'
 import { getProfile } from './routes/auth/get-profile'
 import { errorHandler } from './error-handler'
+import { requestPasswordRecover } from './routes/auth/request-password-recover'
+import { resetPassword } from './routes/auth/reset-password'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -46,6 +48,8 @@ app.register(fastifyCors)
 app.register(createAccount)
 app.register(authenticateWithPassword)
 app.register(getProfile)
+app.register(requestPasswordRecover)
+app.register(resetPassword)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Link Start')
