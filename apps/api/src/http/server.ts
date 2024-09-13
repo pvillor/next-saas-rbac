ced0df79ba7fb87a3139e26d77a79e1c861764e2
paulo.guerra <paulo.guerra@flex-am.com.br>
2024-09-13 15:52:18 -0400

    chore: setup 0.0.0.0 as default api host

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -113,6 +113,6 @@ app.register(getPendingInvites)
 
 app.register(getOrganizationBilling)
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
   console.log('Link Start')
 })
